edc0b7ed4c769ebc3c57ee2e6d0cb8ca1575cd7b
Ivan Chernov <vanyaz158@gmail.com>
2021-10-01 12:27:58 +0300

    Remove memoization

--- lib/umbrellio_utils/semantic_logger/tiny_json_formatter.rb
+++ lib/umbrellio_utils/semantic_logger/tiny_json_formatter.rb
@@ -9,8 +9,6 @@ module UmbrellioUtils
     #   formatter = UmbrellioUtils::SemanticLogger::TinyJsonFormatter.new
     #   SemanticLogger.add_appender(io: $stdout, formatter: formatter)
     class TinyJsonFormatter
-      include Memery
-
       # Formats log structure into the JSON string.
       # @param log [SemanticLogger::Log] log's data structure.
       # @param logger [SemanticLogger::Logger] active logger.
@@ -37,7 +35,7 @@ module UmbrellioUtils
 
       # Calculates MD5 fingerprint for the thread, in which the log was made.
       # @private
-      memoize def thread_fingerprint_for(log)
+      def thread_fingerprint_for(log)
         Digest::MD5.hexdigest("#{log.thread_name}#{Process.pid}")[0...8]
       end
     end
