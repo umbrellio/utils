a439cad8ee9b5fde3d11afbc0d9a5ed13c7f6181
Yuri Smirnov <tycooon@yandex.ru>
2022-07-29 20:12:06 +0300

    rework create_temp_table specs

--- spec/umbrellio_utils/database_spec.rb
+++ spec/umbrellio_utils/database_spec.rb
@@ -2,46 +2,27 @@
 
 describe UmbrellioUtils::Database do
   describe "#create_temp_table" do
-    let(:column_schema) { double("column_schema") }
-    let(:schema) { double("schema") }
-    let(:model) { double("model", table_name: "test_table_name", db_schema: schema) }
-    let(:dataset) { double("dataset", model: model) }
-    let(:sequel) { double("sequel") }
-    let(:table) { double("table") }
-    let(:select_expr) { double("select_expression") }
-
-    # rubocop:disable Naming/VariableNumber
-    let(:table_name) { :temp_test_table_name_16387488000 }
-    # rubocop:enable Naming/VariableNumber
-
-    before { Timecop.freeze("2021-12-06 UTC") }
-
     before do
-      stub_const("DB", double)
-      stub_const("Sequel", sequel)
+      Timecop.freeze("2021-12-06 UTC")
+      allow(DB).to receive(:create_table).with(table_name, unlogged: true).and_call_original
     end
 
-    before do
-      allow(schema).to receive(:[]).with("test_primary_key").and_return(column_schema)
-      allow(column_schema).to receive(:[]).with(:db_type).and_return("test_type")
-
-      allow(DB).to receive(:drop_table?)
-      allow(DB).to receive(:create_table).with(table_name, unlogged: true)
-      allow(DB).to receive(:[]).with(table_name).and_return(table)
-
-      allow(Sequel).to receive(:[]).with("test_table_name").and_return(table)
-
-      allow(table).to receive(:[]).with("test_primary_key").and_return(select_expr)
-      allow(dataset).to receive(:select).with(select_expr).and_return(dataset)
-      allow(table).to receive(:insert).with(dataset)
+    let!(:users) do
+      [
+        User.create(email: "user1@mail.com"),
+        User.create(email: "user2@mail.com"),
+      ]
     end
+
+    let(:table_name) { :temp_users_16387488000 }
 
     subject(:result) do
-      described_class.create_temp_table(dataset, primary_key: "test_primary_key")
+      described_class.create_temp_table(User.dataset, primary_key: :id)
     end
 
     specify do
       expect(result).to eq(table_name)
+      expect(DB[table_name].select_map(:id)).to match_array(users.map(&:id))
     end
   end
 
@@ -55,7 +36,7 @@ describe UmbrellioUtils::Database do
     let(:options) { Hash[] }
 
     let(:users_data) do
-      Array.new(10) { |index| Hash[email: "#{index}@email.com"] }
+      Array.new(10) { |index| Hash[email: "user#{index + 1}@email.com"] }
     end
 
     let(:reversed_emails) { users_data.pluck(:email).reverse }
@@ -63,7 +44,7 @@ describe UmbrellioUtils::Database do
     subject(:result_emails) do
       users = []
 
-      UmbrellioUtils::Database.each_record(User.dataset, **options) do |user|
+      described_class.each_record(User.dataset, **options) do |user|
         users << user
       end
 
