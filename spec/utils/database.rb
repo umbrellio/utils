df3a1d89c073ec0d2ee96b15c9a26b63876cf606
Yuri Smirnov <tycooon@yandex.ru>
2022-07-29 19:28:32 +0300

    wip

---
+++ spec/utils/database.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require "logger"
+
+begin
+  db_name = "umbrellio_utils_test"
+  DB = Sequel.connect(ENV.fetch("DB_URL", "postgres://localhost/#{db_name}"))
+rescue Sequel::DatabaseConnectionError => error
+  puts error
+  abort "You probably need to create a test database. " \
+        "Try running the following command: `createdb #{db_name}`"
+end
+
+DB.logger = Logger.new("log/db.log")
+
+Sequel::Model.db = DB
+
+DB.extension :pg_array
+DB.extension :pg_json
+DB.extension :pg_range
+
+DB.extension :currency_rates
+DB.extension :pg_tools
+DB.extension :slave
+DB.extension :set_local
+DB.extension :synchronize
+
+Sequel.extension :deferrable_foreign_keys
+Sequel.extension :migration
+Sequel.extension :pg_array_ops
+Sequel.extension :pg_json_ops
+Sequel.extension :pg_range_ops
+
+Sequel::Model.plugin :attr_encrypted
+Sequel::Model.plugin :duplicate
+Sequel::Model.plugin :get_column_value
+Sequel::Model.plugin :money_accessors
+Sequel::Model.plugin :store_accessors
+Sequel::Model.plugin :synchronize
+Sequel::Model.plugin :upsert
+Sequel::Model.plugin :with_lock
+
+def clean_database!
+  DB.tables.each do |table_name|
+    DB.drop_table?(table_name, cascade: true)
+  end
+end
