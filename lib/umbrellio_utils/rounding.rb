be06bb87eb7478788b7f8a44b720d9df6a6a9d1b
dmitry.rom <dmitry.rom@cadolabs.io>
2024-02-08 13:26:37 +0300

    Add targets param to super_round method

--- lib/umbrellio_utils/rounding.rb
+++ lib/umbrellio_utils/rounding.rb
@@ -4,6 +4,8 @@ module UmbrellioUtils
   module Rounding
     extend self
 
+    SUPER_ROUND_DEFAULT_TARGETS = [1.0, 1.5, 2.5, 5.0, 10.0].freeze
+
     def fancy_round(number, rounding_method: :round, ugliness_level: 1)
       return 0 unless number.positive?
       log = Math.log(number, 10).floor
@@ -11,7 +13,7 @@ module UmbrellioUtils
       (number * coef).public_send(rounding_method, -log) / coef.to_f
     end
 
-    def super_round(number, rounding_method: :round)
+    def super_round(number, rounding_method: :round, targets: SUPER_ROUND_DEFAULT_TARGETS)
       return 0 unless number.positive?
 
       coef = 10**Math.log(number, 10).floor
@@ -19,7 +21,7 @@ module UmbrellioUtils
 
       best_diff = best_target = nil
 
-      [1.0, 1.5, 2.5, 5.0, 10.0].each do |target|
+      targets.each do |target|
         diff = target - num
 
         next if rounding_method == :ceil && diff.negative?
