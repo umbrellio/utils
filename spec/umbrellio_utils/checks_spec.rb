74b888584b88cf3d12372b7ea80d722e2e56b897
Yuri Smirnov <tycoooon@gmail.com>
2024-10-26 13:02:46 +0300

    rework specs

--- spec/umbrellio_utils/checks_spec.rb
+++ spec/umbrellio_utils/checks_spec.rb
@@ -4,26 +4,21 @@ describe UmbrellioUtils::Checks do
   describe "#valid_email?" do
     subject(:result) { described_class.valid_email?(input) }
 
-    let(:input) { "user@example.com" }
+    expectations = {
+      "user@example.com" => true,
+      "user@one.two42.com" => true,
+      "invalid" => false,
+      123 => false,
+      nil => false,
+    }
 
-    it { is_expected.to eq(true) }
+    expectations.each do |input, expected_result|
+      context "with input #{input.inspect} should return #{expected_result.inspect}" do
+        let(:input) { input }
+        let(:expected_result) { expected_result }
 
-    context "invalid input" do
-      let(:input) { "invalid" }
-
-      it { is_expected.to eq(false) }
-    end
-
-    context "input with subdomains and digits" do
-      let(:input) { "user@one.two42.com" }
-
-      it { is_expected.to eq(true) }
-    end
-
-    context "non-string input" do
-      let(:input) { 123 }
-
-      it { is_expected.to eq(false) }
+        it { is_expected.to eq(expected_result) }
+      end
     end
   end
 end
