60a4ee87febf18cb4b33039f62780180ba82260c
KirIgor <KirIgor@users.noreply.github.com>
2025-12-02 16:00:24 +0600

    fix

--- lib/umbrellio-utils.rb
+++ lib/umbrellio-utils.rb
@@ -5,3 +5,7 @@ require_relative "umbrellio_utils"
 if defined?(Rake)
   Dir[File.join(__dir__, "umbrellio_utils/tasks/**/*.rake")].each { |f| load f }
 end
+
+if defined?(RSpec)
+  Dir[File.join(__dir__, "umbrellio_utils/testing/**/*.rb")].each { |f| load f }
+end
